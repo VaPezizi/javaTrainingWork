65a031a41bdfd5820ed3c52a65ee023173168966
VaPezizi <123574060+VaPezizi@users.noreply.github.com>
2024-04-15 21:52:54 +0300

    Add files via upload

--- javaharjtyo/src/main/java/com/harjtyo/javaFx.java
+++ javaharjtyo/src/main/java/com/harjtyo/javaFx.java
@@ -1,5 +1,15 @@
 package com.harjtyo;
 
+import java.util.ArrayList;
+
+import org.json.JSONObject;
+
+//import java.util.ArrayList;
+//import java.util.Collection;
+//import java.util.Collections;
+
+//import org.json.JSONObject;
+
 import javafx.application.Application;  // sovellus
 import javafx.stage.Stage;              // (pää)ikkuna
 
@@ -7,9 +17,12 @@ import javafx.stage.Stage;              // (pää)ikkuna
 import javafx.scene.Scene;              // näkymä
 import javafx.scene.layout.BorderPane;  // asettelu
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
+import javafx.geometry.HPos;
 //import javafx.geometry.Pos;             // komponentin sijoittelu
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 //import javafx.geometry.Pos;          // komponentin reunukset (padding)
 import javafx.scene.control.Label;      // komponentti (teksti)
 import javafx.scene.control.Button;     // komponentti (painike)
@@ -17,10 +30,10 @@ import javafx.scene.control.Button;     // komponentti (painike)
 //import javafx.scene.layout.StackPane;  
 //import javafx.stage.Stage;  
 import javafx.scene.control.ScrollPane;
-
+import javafx.scene.control.TextField;
 // c, kommentoitu ulos kohdassa d
-import javafx.event.EventHandler;       // tapahtumankäsittely
-import javafx.event.ActionEvent;        // tapahtumankäsittely
+//import javafx.event.EventHandler;       // tapahtumankäsittely
+//import javafx.event.ActionEvent;        // tapahtumankäsittely
 
 public class javaFx extends Application{
     // d
@@ -39,10 +52,12 @@ public class javaFx extends Application{
         Label otsikkoLabel = new Label("Mittaukset");
 
         BorderPane Asettelu = new BorderPane();
+
         Asettelu.setPadding(new Insets(5));
         //noppaAsettelu.setRight(scrollBar);
         Asettelu.setTop(otsikkoLabel);
         
+
         textProcess textHandler = new textProcess(dataProcessor.lueData("./javaharjtyo/src/main/java/com/harjtyo/data.json"));
         Text teksti = new Text();
         teksti.setText(teksti.getText() + textHandler.jArrToText().getText());
@@ -54,23 +69,96 @@ public class javaFx extends Application{
         //Insets buttonInsets = new Insets(5);
         //Creating buttons for different sorting methods
         Button aikaButton = new Button("Aika");
-        Label aikaLabel = new Label("Lajittelu: Aika, Vanhin ensin");
+        Label lajitteluLabel = new Label("Lajittelu: Aika, Uusin ensin");
 
+        //Input field and labels for time input column
+
+        Label input1Label = new Label("Lisää mittaus:");
+
+        
+
+        //TextField aikaTextField = new TextField("");
+        //aikaTextField.setMaxWidth(170);
+
+        //Input field and labels for dampness
+        Label input2Label = new Label("Kosteus: ");
+        TextField kosteusTextField = new TextField();
+        kosteusTextField.setMaxWidth(170);
+        //Input fields and labels for temperature
+        Label input3Label = new Label("Lampotila: ");
+        TextField lampoTextField = new TextField();
+        lampoTextField.setMaxWidth(170);
+        //Input field and label for pressure:
+        Label input4Label = new Label("Paine: ");
+        TextField paineTextField = new TextField();
+        paineTextField.setMaxWidth(170);
+
+        //Label and input field for removing data
+        //I decided to make the data removal be done by entering a specific time
+        Label input5Label = new Label("Tekstin poisto ajan mukaan:");
+        TextField deleteTextField = new TextField();
+        deleteTextField.setMaxWidth(170);
+
+        //Label and input for fetching data by date
+        Label dateDataLabel = new Label("Hae tietyn ajan mittaukset:");
+        ArrayList<JSONObject> JsonArray = textHandler.getArrayList();
+        TextField dateDataField = new TextField(JsonArray.get(JsonArray.size() - 1).getString("Aika"));
+        dateDataField.setMaxWidth(170);
+        TextField dateDataField2 = new TextField(JsonArray.get(0).getString("Aika"));
+        //Label fetchedData = new Label("Aika: (Huom annettava muodossa: 'year/month/day hour:minute:second)");
+        //fetchedData.setMinWidth(170);
+        //fetchedData.setMaxWidth(300);
+        //Text for telling if text removal worked
+        Text deleteText = new Text("");
+
+
+        Button dataRemoveButton = new Button("Poista");
+
+        Button dateDataButton = new Button("Hae");
+
+        Button dataAddButton = new Button("Lisaa");
+        
         Button kosteusButton = new Button("Kosteus");
 
         Button lampotilaButton = new Button("Lampotila");
 
+        Button paineButton = new Button("Paine");
 
         FlowPane tekstit = new FlowPane();
-        tekstit.setPadding(new Insets(5));
-        tekstit.setHgap(10);
-        tekstit.getChildren().addAll(aikaButton, kosteusButton, lampotilaButton);
-        tekstit.getChildren().addAll(aikaLabel);
+        tekstit.setPadding(new Insets(10));
+        tekstit.setHgap(20);
+        tekstit.getChildren().addAll(aikaButton, kosteusButton, lampotilaButton, paineButton);
+        tekstit.getChildren().addAll(lajitteluLabel);
+        tekstit.setMaxWidth(500);
+        //tekstit.getChildren().addAll(inputLabel, textField);
 
-        Asettelu.setLeft(tekstit);
+        FlowPane datainFlowPane = new FlowPane(Orientation.VERTICAL);
+        datainFlowPane.setVgap(5);
+        datainFlowPane.setColumnHalignment(HPos.LEFT);
+        datainFlowPane.getChildren().add(tekstit);
+        datainFlowPane.setMaxWidth(500);
+        datainFlowPane.setPrefWidth(500);
+
+        HBox aikaValiHBox = new HBox();
+        aikaValiHBox.setPadding(new Insets(10));
+        aikaValiHBox.setSpacing(10);
+        aikaValiHBox.getChildren().addAll(dateDataField, new Text("-"), dateDataField2);
 
 
-        Scene nakyma = new Scene(Asettelu, 800, 720);
+        datainFlowPane.getChildren().addAll(input1Label, input2Label, kosteusTextField,
+        input3Label, lampoTextField, input4Label, paineTextField, dataAddButton, input5Label, 
+        deleteTextField, dataRemoveButton, deleteText, dateDataLabel, aikaValiHBox, dateDataButton);
+
+
+        //datainFlowPane.getChildren().add(aikaTextField);
+        datainFlowPane.setMaxWidth(50);
+
+        //Asettelu.setCenter(aikaInfo);
+        //Asettelu.setCenter(mittausInputFlowPane);
+        Asettelu.setLeft(datainFlowPane);
+
+        //teksti.setText(textHandler.textSort("Aika").getText());
+        Scene nakyma = new Scene(Asettelu, 1280, 720);
         
         ikkuna.setScene(nakyma);
         ikkuna.setTitle("testi");
@@ -80,16 +168,78 @@ public class javaFx extends Application{
         //Lamda operation for sorting data by time
         aikaButton.setOnAction((event -> {
                 //Text teksti = textHandler.textSort("Aika");
-            if (aikaLabel.getText() == "Lajittelu: Aika, Vanhin ensin") {
+            if (lajitteluLabel.getText() != "Lajittelu: Aika, Uusin ensin") {
                 teksti.setText(textHandler.textSort("Aika").getText());
-                aikaLabel.setText("Lajittelu: Aika, Uusin ensin");
+                lajitteluLabel.setText("Lajittelu: Aika, Uusin ensin");
             }else{
                 teksti.setText(textHandler.textSort("AikaReverse").getText());
-                aikaLabel.setText("Lajittelu: Aika, Vanhin ensin");
+                lajitteluLabel.setText("Lajittelu: Aika, Vanhin ensin");
             }
             //silmaluku.setText(String.valueOf(n1.getSilmaluku()));
             }
         ));
+        kosteusButton.setOnAction((event -> {
+            //Text teksti = textHandler.textSort("Aika");
+            if (lajitteluLabel.getText() != "Lajittelu: Kosteus, Isoin ensin") {
+                teksti.setText(textHandler.textSort("Kosteus").getText());
+                lajitteluLabel.setText("Lajittelu: Kosteus, Isoin ensin");
+            }else{
+                teksti.setText(textHandler.textSort("KosteusReverse").getText());
+                lajitteluLabel.setText("Lajittelu: Kosteus, Pienin ensin");
+            }
+            //silmaluku.setText(String.valueOf(n1.getSilmaluku()));
+            }
+        ));
+        lampotilaButton.setOnAction((event -> {
+            //Text teksti = textHandler.textSort("Aika");
+            if (lajitteluLabel.getText() != "Lajittelu: Lampotila, Isoin ensin") {
+                teksti.setText(textHandler.textSort("Lampotila").getText());
+                lajitteluLabel.setText("Lajittelu: Lampotila, Isoin ensin");
+            }else{
+                teksti.setText(textHandler.textSort("LampotilaReverse").getText());
+                lajitteluLabel.setText("Lajittelu: Lampotila, Pienin ensin");
+            }
+            //silmaluku.setText(String.valueOf(n1.getSilmaluku()));
+            }
+        ));
+        paineButton.setOnAction((event -> {
+            //Text teksti = textHandler.textSort("Aika");
+            if (lajitteluLabel.getText() != "Lajittelu: Paine, Isoin ensin") {
+                teksti.setText(textHandler.textSort("Paine").getText());
+                lajitteluLabel.setText("Lajittelu: Paine, Isoin ensin");
+            }else{
+                teksti.setText(textHandler.textSort("PaineReverse").getText());
+                lajitteluLabel.setText("Lajittelu: Paine, Pienin ensin");
+            }
+            //silmaluku.setText(String.valueOf(n1.getSilmaluku()));
+            }
+        ));
+        dataAddButton.setOnAction((event -> {
+            //String aika = aikaTextField.getText();
+            String kosteus = kosteusTextField.getText();
+            String lampo = lampoTextField.getText();
+            String paine = paineTextField.getText();
+            String regex = "\\d+";
+            if(kosteus.matches(regex) && lampo.matches(regex) && paine.matches(regex)){
+                textHandler.addData(kosteus, lampo, paine);
+                teksti.setText(textHandler.textSort("Aika").getText());
+                lajitteluLabel.setText("Lajittelu: Aika, Uusin ensin");
+            }else{
+                input1Label.setText("Virheellinen Syöte!");
+            }
+        }));
+        dataRemoveButton.setOnAction((event -> {
+            String aika = deleteTextField.getText();
+            deleteText.setText(textHandler.removeData(aika));
+
+            teksti.setText(textHandler.textSort("Aika").getText());
+            lajitteluLabel.setText("Lajittelu: Aika, Uusin ensin");
+        }));
+        dateDataButton.setOnAction((event -> {
+            //fetchedData.setText(textHandler.searchData(dateDataField.getText(), dateDataField2.getText()));
+            teksti.setText(textHandler.searchData(dateDataField.getText(), dateDataField2.getText()).getText());
+
+        }));
         /*
         
         noppaAsettelu.setTop(tahot);
@@ -149,13 +299,13 @@ public class javaFx extends Application{
     }
 
     // d
-    public void paivitaNoppa(BorderPane noppaAsettelu) {
+    /*public void paivitaNoppa(BorderPane noppaAsettelu) {
         Label tahot = (Label)noppaAsettelu.getTop();
         Label silmaluku = (Label)noppaAsettelu.getCenter();
         
         //tahot.setText("d" + this.n1.getTahoja());
         //silmaluku.setText(String.valueOf(this.n1.getSilmaluku()));
-    }
+    }*/
 
     public static void main(String[] args) {
         //launch(JavaFxNoppa.class, args);
--- javaharjtyo/src/main/java/com/harjtyo/textProcess.java
+++ javaharjtyo/src/main/java/com/harjtyo/textProcess.java
@@ -1,6 +1,9 @@
 package com.harjtyo;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.text.DecimalFormat;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 import javafx.scene.text.Text;
@@ -9,7 +12,6 @@ import java.util.Collections;
 import java.util.Comparator;
 //import java.util.HashMap;
 //import java.util.List;
-import java.time.format.DateTimeFormatter;  
 
 public class textProcess {
     private JSONArray jArr;
@@ -22,16 +24,71 @@ public class textProcess {
     public Text jArrToText(){
         //Converts JSONArray to Text for the GUI
         Text teksti = new Text();
-
-        for(int i = 0; i < this.jArr.length();i++){      
-            //jArray.get for some reason returns an Object and not an JSONObject, so casting i to JSONObj here
-            JSONObject jObj = ((JSONObject) jArr.get(i));
-            DecimalFormat formatter = new DecimalFormat("#0.0000000");
-            teksti.setText(teksti.getText() + jObj.getString("Aika") + "\t" + formatter.format(jObj.getDouble("Kosteus")) + "\t" + formatter.format(jObj.getDouble("Lampotila")) + "\t" + formatter.format(jObj.getDouble("Paine")) + "\t" +  "\n");
+        try{
+            for(int i = 0; i < this.jArr.length();i++){      
+                //jArray.get for some reason returns an Object and not an JSONObject, so casting i to JSONObj here
+                JSONObject jObj = ((JSONObject) jArr.get(i));
+                DecimalFormat formatter = new DecimalFormat("#0.0000000");
+                teksti.setText(teksti.getText() + jObj.getString("Aika") + "\t" + formatter.format(jObj.getDouble("Kosteus")) + "\t" + formatter.format(jObj.getDouble("Lampotila")) + "\t" + formatter.format(jObj.getDouble("Paine")) + "\t" +  "\n");
+            }
+        }catch (Exception e){
+            return teksti;
         }
         return teksti;
     }
 
+    public Text searchData(String aika, String aika2){
+        /*for (JSONObject jsonObject : JArrList) {
+            if(jsonObject.getString("Aika").equals(aika)){
+                NumberFormat formatter = new DecimalFormat("#0.00");
+                String str = "Aika : " + jsonObject.getString("Aika") + " Kosteus: " + String.valueOf(formatter.format(jsonObject.getDouble("Kosteus"))) + " Lampotila: " + String.valueOf(formatter.format(jsonObject.getDouble("Lampotila"))) + " Paine:  " + String.valueOf(formatter.format(jsonObject.getDouble("Paine")));
+                return str;
+            }
+        }*/
+        Text teksti = new Text();
+        Long aikaLong = Long.valueOf(aika.replaceAll("[^0-9]", ""));
+        Long aika2Long = Long.valueOf(aika2.replaceAll("[^0-9]", "")) + 1;
+        DecimalFormat formatter = new DecimalFormat("#0.0000000");
+        for(JSONObject jsonObject:JArrList){
+            Long jAika = Long.valueOf(jsonObject.getString("Aika").replaceAll("[^0-9]", ""));
+            if(aikaLong <= jAika && (aika2Long - jAika > 0)){
+                teksti.setText(teksti.getText() + jsonObject.getString("Aika") + "\t" + formatter.format(jsonObject.getDouble("Kosteus")) + "\t" + formatter.format(jsonObject.getDouble("Lampotila")) + "\t" + formatter.format(jsonObject.getDouble("Paine")) + "\t" +  "\n");
+            }
+        }
+        return teksti;
+    }
+
+    public void addData(String kosteus, String lampo, String paine){
+        JSONObject data = new JSONObject();
+        LocalDateTime aika = LocalDateTime.now();
+        data.put("Aika", DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(aika));
+        data.put("Kosteus", kosteus);
+        data.put("Lampotila", lampo);
+        data.put("Paine", paine);
+        
+        this.JArrList.add(data);
+        this.jArr.put(data);
+        dataProcessor.kirjoitaTiedostoon(this.jArr);
+    }
+    /*
+     * Removes a specified item from the data file and 
+     */
+    public String removeData(String aika){
+        for (JSONObject jsonObject : JArrList) {
+            //System.out.println(jsonObject.getString("Aika"));
+            if(jsonObject.getString("Aika").equals(aika)){
+                //System.out.println(aika);
+                JArrList.remove(jsonObject);
+                this.jArr = new JSONArray(JArrList);
+                dataProcessor.kirjoitaTiedostoon(jArr);
+                return "Succesfully removed";
+            }
+        }
+        return "Removal failed, time: " + aika + " not found.";
+    }
+    /*
+     * Returns a JSONObject ArrayList from the JSONArray
+     */
     public ArrayList<JSONObject> JsonArrayToList(){
         ArrayList<JSONObject> lista = new ArrayList<JSONObject>();
         for (int i = 0; i<this.jArr.length();i++) {
@@ -40,38 +97,54 @@ public class textProcess {
         }
         return lista;
     }
-    public JSONArray ArrListToJsnArr(ArrayList<JSONObject> ArrList){
-        JSONArray jarr = new JSONArray(); 
-        for (JSONObject jsonObject : ArrList) {
-            jarr.put(jsonObject);
-        }
-        return jarr;
+    public ArrayList<JSONObject> getArrayList(){
+        return this.JArrList;
     }
+    public JSONArray getJsonArray(){
+        return this.jArr;
+    }
+    /*public JSONArray ArrListToJsnArr(ArrayList<JSONObject> ArrList){
+       JSONArray jarr = new JSONArray(ArrList); 
+        //for (JSONObject jsonObject : ArrList) { 
+        //    jarr.put(jsonObject);
+        //}
+        return jarr;
+    }*/
     
 
+    //I realised way after doing this whole sorting thing, that i had a way better way of doing this,
+    //which should be faster, but im too lazy, as i would need to do this whole method completely differently.
     public Text textSort(String nappi){
         //Taking the current JSONArray object and making an ArrayList copy of it
         this.JArrList = JsonArrayToList();
         switch (nappi) {
             case "Aika":
-                //ArrayList<JSONObject> Lista = new ArrayList<>();
-                //DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-                //int last = 0;
-                //System.out.println(this.JArrList);
                 Collections.sort(this.JArrList, new AikaCompare());
                 break;
             case "AikaReverse":
-                //ArrayList<JSONObject> Lista2 = new ArrayList<>();
-                //DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-                //int last = 0;
-                Collections.sort(this.JArrList, new AikaCompare());
+                //Collections.sort(this.JArrList, new AikaCompare());
                 Collections.reverse(this.JArrList);
                 break;
             case "Kosteus":
-
+                Collections.sort(this.JArrList, new KosteusCompare());
+                break;
+            case "KosteusReverse":
+                //Collections.sort(this.JArrList, new KosteusCompare());
+                Collections.reverse(this.JArrList);
                 break;
             case "Lampotila":
-
+                Collections.sort(this.JArrList, new LampoCompare());
+                break;
+            case "LampotilaReverse":
+                //Collections.sort(this.JArrList, new LampoCompare());
+                Collections.reverse(this.JArrList);
+                break;
+            case "Paine":
+                Collections.sort(this.JArrList, new PaineCompare());
+                break;
+            case "PaineReverse":
+                //Collections.sort(this.JArrList, new LampoCompare());
+                Collections.reverse(this.JArrList);
                 break;
             default:
                 break;
@@ -79,19 +152,45 @@ public class textProcess {
 
         //System.out.println(this.JArrList);
         //Updating the current JSONArray to match the JSONObject ArrayList
-        this.jArr = this.ArrListToJsnArr(JArrList);
+        this.jArr = new JSONArray(JArrList);
         return this.jArrToText();
     }
 }
 class AikaCompare implements Comparator<JSONObject>{
     @Override
     public int compare(JSONObject lhs, JSONObject rhs){
-        //Yeah yeah i know it looks but, but works ;D
+        //Yeah yeah i know it looks bad but, but works ;D Edit: used to look much worse but still bad
         Long lhs2 = Long.valueOf(lhs.getString("Aika").replaceAll("[^0-9]", ""));
         Long rhs2 = Long.valueOf(rhs.getString("Aika").replaceAll("[^0-9]", ""));
 
         return lhs2 > rhs2 ? -1 : lhs2 < rhs2 ? 1 : 0;
         //System.out.println(i);
-        
+    }
+}
+class KosteusCompare implements Comparator<JSONObject>{
+    @Override
+    public int compare(JSONObject lhs, JSONObject rhs){
+        Double lhs2 = lhs.getDouble("Kosteus");
+        Double rhs2 = rhs.getDouble("Kosteus");
+        return lhs2 > rhs2 ? -1 : lhs2 < rhs2 ? 1 : 0;
+    }
+}
+/*This is such a stupid way to do this, i just can't figure out
+How to get any extra arguments there, would save a bunch, if i just
+could get the JSON Key as an argument*/
+class LampoCompare implements Comparator<JSONObject>{
+    @Override
+    public int compare(JSONObject lhs, JSONObject rhs){
+        Double lhs2 = lhs.getDouble("Lampotila");
+        Double rhs2 = rhs.getDouble("Lampotila");
+        return lhs2 > rhs2 ? -1 : lhs2 < rhs2 ? 1 : 0;
+    }
+}
+class PaineCompare implements Comparator<JSONObject>{
+    @Override
+    public int compare(JSONObject lhs, JSONObject rhs){
+        Double lhs2 = lhs.getDouble("Paine");
+        Double rhs2 = rhs.getDouble("Paine");
+        return lhs2 > rhs2 ? -1 : lhs2 < rhs2 ? 1 : 0;
     }
 }
